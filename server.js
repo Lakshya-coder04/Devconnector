d0b86a9ea5ff8a60a09c76ee1cb47958ee11fc98
Lakshya Birla <Lakshyabirla7cr@gmail.com>
2023-08-22 01:56:27 -0600

    2nd Commit
    db, models, routes

--- server.js
+++ server.js
@@ -4,13 +4,29 @@
  the process of handling HTTP requests, routing, middleware, and more.*/
 const express = require('express');
 
+//get databse function that we made in db.js
+
+const connectDB = require('./config/db');
+
 /*This line creates an instance of the Express application 
 by calling the imported 'express' function. 
 This instance represents your web application 
 and will be used to define routes, handle requests, and set up middleware. */
 const app = express();
 
+//connect database
+connectDB();
+
+// init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000; //default to 5000 when there is no en-variable  set
 
